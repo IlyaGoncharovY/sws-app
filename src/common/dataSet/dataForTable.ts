2691cd943dcb78af4f6a7fa901e1aa6c428a83c8
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-12-26 02:43:40 +0300

    feat(dataSet) data for render in page

---
+++ src/common/dataSet/dataForTable.ts
@@ -0,0 +1,37 @@
+type dataType = {
+    id: number
+    title: string
+}
+export const DataForLinesTable: dataType[] = [
+  {id: 1, title: 'Уровень'},
+  {id: 2, title: 'Наименование работ'},
+  {id: 3, title: 'Затраты на оборудование'},
+  {id: 4, title: 'Предполагаемая прибыль'},
+  {id: 5, title: 'Зарплата механизатора'},
+  {id: 6, title: 'Основные затраты'},
+];
+
+export const DataForMainTable: dataType[] = [
+  {id: 1, title: 'По проекту'},
+  {id: 2, title: 'Обьекты'},
+  {id: 3, title: 'РД'},
+  {id: 4, title: 'МТО'},
+  {id: 5, title: 'СМР'},
+  {id: 6, title: 'График'},
+  {id: 7, title: 'МиМ'},
+  {id: 8, title: 'Рабочие'},
+  {id: 9, title: 'Капвложения'},
+  {id: 10, title: 'Бюджет'},
+  {id: 11, title: 'Финансирование'},
+  {id: 12, title: 'Панорамы'},
+  {id: 13, title: 'Камеры'},
+  {id: 14, title: 'Поручения'},
+  {id: 15, title: 'Контрагенты'},
+];
+
+export const DataForHeader: dataType[] = [
+  {id: 1, title: '[]'},
+  {id: 2, title: '<='},
+  {id: 3, title: 'Промсотр'},
+  {id: 4, title: 'Управление'},
+];
