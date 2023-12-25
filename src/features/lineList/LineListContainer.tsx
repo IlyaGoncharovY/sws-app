8702a9725d1c8dfcce2964ee0bbbe183214f501c
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-12-26 02:47:51 +0300

    fix(LineList/item/style) refactor render button, data of table, loader

--- src/features/lineList/LineListContainer.tsx
+++ src/features/lineList/LineListContainer.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import {SWSApi} from '../../api/SWSApi';
-import {RequestAddNewLineType} from './types';
 import LineListItem from './item/LineListItem';
+
+import s from './LineListContainer.module.scss';
+import HeaderLines from './headerLines/HeaderLines';
 
 const eID = process.env.REACT_APP_E_ID;
 
@@ -14,47 +16,31 @@ export default function LineListContainer() {
     error,
   } = SWSApi.useGetAllLinesQuery(+eID!);
 
-  const [addNewLine,
-    {
-      error: addNewLineError,
-      isLoading: addNewLineIsLoading,
-    },
-  ] = SWSApi.useAddNewLineMutation();
-
   if (isLoading) {
-    return <h1>...Загрузка линий</h1>;
+    return (
+      <>
+        <h1>...Загрузка линий</h1>
+        <div className={s.lineListContainer}>
+          <table>
+            <HeaderLines />
+          </table>
+        </div>
+      </>
+    );
   }
 
   if (error) {
     return <h1>{`Произошла ошибка ${error}`}</h1>;
   }
 
-  const handleAddNewLine = async () => {
-    const newLine: RequestAddNewLineType = {
-      equipmentCosts: 0,
-      estimatedProfit: 0,
-      machineOperatorSalary: 0,
-      mainCosts: 0,
-      materials: 0,
-      mimExploitation: 0,
-      overheads: 0,
-      parentId: null,
-      rowName: `New Line ${Lines && Lines.length + 1}`,
-      salary: 0,
-      supportCosts: 0,
-    };
-    await addNewLine({newLine: newLine, eID: +eID!});
-  };
-
   return (
-    <div>
-      LineListContainer
-      {Lines && Lines.map((line) =>
-        <LineListItem key={line.id} line={line}/>,
-      )}
-      <button onClick={handleAddNewLine} disabled={addNewLineIsLoading}>add lines</button>
-      {addNewLineIsLoading && <div>....добавление</div>}
-      {addNewLineError && <h1>Ошибка при добавлении линии</h1>}
+    <div className={s.lineListContainer}>
+      <table>
+        <HeaderLines/>
+        {Lines && Lines.map((line) =>
+          <LineListItem key={line.id} line={line} eID={+eID!}/>,
+        )}
+      </table>
     </div>
   );
 }
--- src/features/lineList/item/LineListItem.tsx
+++ src/features/lineList/item/LineListItem.tsx
@@ -1,24 +1,67 @@
 import React from 'react';
 
-import {LinesListResponseChild} from '../types';
+import {LinesListResponseChild, RequestAddNewLineType} from '../types';
+
+import {SWSApi} from '../../../api/SWSApi';
 
 interface ILineListItem {
     line: LinesListResponseChild
+    eID: number
 }
 
-const LineListItem: React.FC<ILineListItem> = ({ line}) => {
+const LineListItem: React.FC<ILineListItem> = ({ line, eID}) => {
+
+  const [addNewLine,
+    {
+      error: addNewLineError,
+      isLoading: addNewLineIsLoading,
+    },
+  ] = SWSApi.useAddNewLineMutation();
+
+  const handleAddNewLine = async () => {
+    const newLine: RequestAddNewLineType = {
+      equipmentCosts: 0,
+      estimatedProfit: 0,
+      machineOperatorSalary: 0,
+      mainCosts: 0,
+      materials: 0,
+      mimExploitation: 0,
+      overheads: 0,
+      parentId: null,
+      rowName: `New Line ${line.id}`,
+      salary: 0,
+      supportCosts: 0,
+    };
+    await addNewLine({newLine: newLine, eID: +eID!});
+  };
 
   return (
-    <div>
-        LineListItem
-      <ul>
-        <li>rowName: {line.rowName}</li>
-        <li>equipmentCosts: {line.equipmentCosts}</li>
-        <li>estimatedProfit: {line.estimatedProfit}</li>
-        <li>total: {line.total}</li>
-        <li>id: {line.id}</li>
-      </ul>
-    </div>
+    <tbody>
+      <tr>
+        <td>
+          <button onClick={handleAddNewLine} disabled={addNewLineIsLoading}>add lines</button>
+          {addNewLineIsLoading && <div>....добавление</div>}
+          {addNewLineError && <h1>Ошибка при добавлении линии</h1>}
+          <button>del</button>
+        </td>
+        <td>
+          {line.rowName}
+          {line.id}
+        </td>
+        <td>
+          {line.equipmentCosts}
+        </td>
+        <td>
+          {line.estimatedProfit}
+        </td>
+        <td>
+          {line.machineOperatorSalary}
+        </td>
+        <td>
+          {line.mainCosts}
+        </td>
+      </tr>
+    </tbody>
   );
 };
 
