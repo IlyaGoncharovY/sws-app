7245266ee7f5754f37187c8830e1afb2ae32e212
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-12-27 00:39:43 +0300

    fix(LineListItem) refactor logic for change body

--- src/features/lineList/item/LineListItem.tsx
+++ src/features/lineList/item/LineListItem.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
-
-import {LinesListResponseChild, RequestAddNewLineType} from '../types';
+import React, {useState} from 'react';
 
 import {SWSApi} from '../../../api/SWSApi';
+import {LinesListResponseChild, RequestAddNewLineType} from '../types';
 
 interface ILineListItem {
-  line: LinesListResponseChild
-  eID: number
+    line: LinesListResponseChild
+    eID: number
 }
 
-const LineListItem: React.FC<ILineListItem> = ({ line, eID}) => {
+const LineListItem: React.FC<ILineListItem> = ({line, eID}) => {
+
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+
+  const [updatedLine, setUpdatedLine] = useState<RequestAddNewLineType>({
+    equipmentCosts: line.equipmentCosts,
+    estimatedProfit: line.estimatedProfit,
+    machineOperatorSalary: line.machineOperatorSalary,
+    mainCosts: line.mainCosts,
+    materials: 0,
+    mimExploitation: 0,
+    overheads: 0,
+    parentId: line.id,
+    rowName: line.rowName,
+    salary: 0,
+    supportCosts: 0,
+  });
 
   const [addNewLine,
     {
@@ -18,9 +33,17 @@ const LineListItem: React.FC<ILineListItem> = ({ line, eID}) => {
     },
   ] = SWSApi.useAddNewLineMutation();
 
-  const [deleteLine] = SWSApi.useDeleteLinesMutation();
+  const [deleteLine,
+    {
+      isLoading: deleteLineIsLoading,
+    },
+  ] = SWSApi.useDeleteLinesMutation();
 
-  const [updateLine, {}] = SWSApi.useUpdateLineMutation();
+  const [updateLine,
+    {
+      isLoading: updateLineIsLoading,
+    },
+  ] = SWSApi.useUpdateLineMutation();
 
   const handleAddNewLine = async () => {
     const newLine: RequestAddNewLineType = {
@@ -40,51 +63,107 @@ const LineListItem: React.FC<ILineListItem> = ({ line, eID}) => {
   };
 
   const handleUpdateLine = async () => {
+    await updateLine({ updatedLine, eID: +eID!, rID: line.id });
+    setIsEditing(false);
+  };
 
-    const updatedLine: RequestAddNewLineType = {
-      equipmentCosts: 0,
-      estimatedProfit: 0,
-      machineOperatorSalary: 0,
-      mainCosts: 0,
-      materials: 0,
-      mimExploitation: 0,
-      overheads: 0,
-      parentId: line.id,
-      rowName: `update line${line.id}`,
-      salary: 0,
-      supportCosts: 0,
-    };
+  const handleDoubleClick = () => {
+    setIsEditing(true);
+  };
 
-    await updateLine({updatedLine: updatedLine, eID: +eID!, rID: line.id});
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>, property: string) => {
+    setUpdatedLine((prevLine) => ({
+      ...prevLine,
+      [property]: Number(event.target.value),
+    }));
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleUpdateLine();
+    }
   };
 
   const deleteLineHandler = (eID: number, rID: number) => {
     deleteLine({eID, rID});
   };
 
-  // const updatedLineHandler = (updatedLine: RequestAddNewLineType, eID: number, rID: number) => {
-  //   updateLine({updatedLine, eID, rID});
-  // };
-
   return (
     <>
       <tr>
         <td>
-          <button onClick={handleAddNewLine} disabled={addNewLineIsLoading}>
-              Add Lines
+          <button onClick={handleAddNewLine} disabled={addNewLineIsLoading || isEditing}>
+                        Add Lines
           </button>
           {addNewLineIsLoading && <div>....добавление</div>}
           {addNewLineError && <h1>Ошибка при добавлении линии</h1>}
-          <button onClick={() => deleteLineHandler(eID, line.id)}>Delete</button>
+          <button onClick={() => deleteLineHandler(eID, line.id)} disabled={deleteLineIsLoading}>Delete</button>
+          {deleteLineIsLoading && <div>...удаление</div>}
         </td>
-        <td>{line.rowName}</td>
-        <td>{line.equipmentCosts}</td>
-        <td>{line.estimatedProfit}</td>
-        <td>{line.machineOperatorSalary}</td>
-        <td>{line.mainCosts}</td>
+        {updateLineIsLoading && <div>...обнавление</div>}
+        <td onDoubleClick={handleDoubleClick}>
+          {isEditing ? (
+            <input
+              type="text"
+              value={updatedLine.rowName}
+              onChange={(e) => handleInputChange(e, 'rowName')}
+              onKeyPress={handleKeyPress}
+            />
+          ) : (
+            line.rowName
+          )}
+        </td>
+        <td onDoubleClick={handleDoubleClick}>
+          {isEditing ? (
+            <input
+              type="text"
+              value={updatedLine.equipmentCosts}
+              onChange={(e) => handleInputChange(e, 'equipmentCosts')}
+              onKeyPress={handleKeyPress}
+            />
+          ) : (
+            line.equipmentCosts
+          )}
+        </td>
+        <td onDoubleClick={handleDoubleClick}>
+          {isEditing ? (
+            <input
+              type="text"
+              value={updatedLine.estimatedProfit}
+              onChange={(e) => handleInputChange(e, 'estimatedProfit')}
+              onKeyPress={handleKeyPress}
+            />
+          ) : (
+            line.estimatedProfit
+          )}
+        </td>
+        <td onDoubleClick={handleDoubleClick}>
+          {isEditing ? (
+            <input
+              type="text"
+              value={updatedLine.machineOperatorSalary}
+              onChange={(e) => handleInputChange(e, 'machineOperatorSalary')}
+              onKeyPress={handleKeyPress}
+            />
+          ) : (
+            line.machineOperatorSalary
+          )}
+        </td>
+        <td onDoubleClick={handleDoubleClick}>
+          {isEditing ? (
+            <input
+              type="text"
+              value={updatedLine.mainCosts}
+              onChange={(e) => handleInputChange(e, 'mainCosts')}
+              onKeyPress={handleKeyPress}
+            />
+          ) : (
+            line.mainCosts
+          )}
+        </td>
       </tr>
       {line.child?.map((childLine) => (
-        <LineListItem key={childLine.id} line={childLine} eID={eID} />
+        <LineListItem key={childLine.id} line={childLine} eID={eID}/>
       ))}
     </>
   );
