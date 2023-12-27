9f41b9173215fc2a059f1211bca9d6241cb62897
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-12-28 00:04:25 +0300

    fix(header) use img

--- src/features/header/HeaderContainer.tsx
+++ src/features/header/HeaderContainer.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
 import {DataForHeader} from '../../common/dataSet/dataForTable';
+import menuIcon from '../../assects/img/burger.png';
+import arrowImg from '../../assects/img/arrowLeft.png';
 
-import s from './HedaerContainer.module.scss';
+import s from './HeaderContainer.module.scss';
 
 export default function HeaderContainer() {
   return (
     <div className={s.headerContainer}>
+
+      <img src={`${menuIcon}`} alt="burger_menu"/>
+      <img src={`${arrowImg}`} alt="arrow_left"/>
+
       {DataForHeader.map((el) =>
         <div key={el.id} className={s.headerTitle}>{el.title}</div>,
       )}
